da93970250ff493eaee723daff2e508a2cc512fc
Ernesto J Rodriguez <vela1606@gmail.com>
2016-05-10 22:28:42 -0400

    compiled

--- app/app.js
+++ app/app.js
@@ -39001,6 +39001,73 @@ Ext.define('App.controller.Support', {
 
 });
 
+Ext.define('App.controller.Theme', {
+	extend: 'Ext.app.Controller',
+
+	refs: [
+		{
+			ref: 'viewport',
+			selector: 'viewport'
+		}
+	],
+
+	init: function(){
+		var me = this;
+
+		me.control({
+			'#AppThemeSwitcher': {
+				click: me.onAppThemeSwitcherClick,
+				beforerender: me.onAppThemeSwitcherBeforeRender
+			}
+		});
+
+	},
+
+	onAppThemeSwitcherBeforeRender: function(btn){
+
+		btn.action = g('mdtimeline_theme');
+		btn.setText(btn.action == 'dark' ? _('light_theme') : _('dark_theme'));
+
+
+		say('onAppThemeSwitcherBeforeRender');
+		say(btn.action);
+	},
+
+	onAppThemeSwitcherClick: function(btn){
+
+		var me = this;
+
+		Ext.Msg.show({
+			title: _('wait'),
+			msg: _('theme_change_warning'),
+			buttons: Ext.Msg.YESNO,
+			icon: Ext.Msg.QUESTION,
+			fn: function(answer){
+				if(answer == 'yes'){
+					if(btn.action == 'dark'){
+						me.goLight(btn);
+					}else{
+						me.goDark(btn);
+					}
+				}
+			}
+		});
+	},
+
+	goLight: function(btn){
+		btn.action = 'light';
+		Ext.util.Cookies.set('mdtimeline_theme', 'light', Ext.Date.add(new Date(), Ext.Date.YEAR, 1));
+		window.location.reload();
+	},
+
+	goDark: function(btn){
+		btn.action = 'dark';
+		Ext.util.Cookies.set('mdtimeline_theme', 'dark', Ext.Date.add(new Date(), Ext.Date.YEAR, 1));
+		window.location.reload();
+	}
+
+});
+
 Ext.define('App.controller.patient.ActiveProblems', {
 	extend: 'Ext.app.Controller',
 	requires: [
@@ -57866,6 +57933,11 @@ Ext.define('App.view.Viewport', {
 				    handler: function(){
 					    me.nav.navigateTo('App.view.miscellaneous.MyAccount');
 				    }
+			    },
+			    {
+				    text: _('light_theme'),
+				    itemId:'AppThemeSwitcher',
+				    action: 'light'
 			    },
 			    {
 				    text: _('logout'),

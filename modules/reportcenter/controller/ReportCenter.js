4bd215e1b438820618148bb9b6b47ccd3cde83a0
Gino Rivera <hrivera@tranextgen.com>
2016-06-21 15:40:46 -0400

    (a)(14) Patient List Creation: - Added some translations - Increased the Height of the Report Window

--- modules/reportcenter/controller/ReportCenter.js
+++ modules/reportcenter/controller/ReportCenter.js
@@ -114,13 +114,12 @@ Ext.define('Modules.reportcenter.controller.ReportCenter', {
         );
 
         // Configure the initial report panel layout, and finally show it.
-        reportWindow.setHeight(600);
+        reportWindow.setHeight(700);
         reportWindow.setWidth(me.reportInformation.reportWindowWidth);
         reportWindow.doLayout();
         this.getReportFilterPanel().setWidth(me.reportInformation.filterPanelWidth);
         this.getFilterDisplayPanel().setHeight(me.reportInformation.filterDisplayHeight);
         reportWindow.setTitle(_('report_window') + ' ( ' + me.reportInformation.title + ' )');
-        reportWindow.render(Ext.getBody());
         reportWindow.show();
     },
 

719430630b87d259ba70454d834b3ec95dae47c7
Ernesto J Rodriguez <vela1606@gmail.com>
2016-07-19 10:24:55 -0400

    session changes

--- data/router.php
+++ data/router.php
@@ -33,9 +33,9 @@ header("Pragma: no-cache"); // HTTP 1.0.
 session_cache_limiter('private');
 session_name('mdTimeLine');
 session_start();
-if(session_status() === PHP_SESSION_ACTIVE ){
-	session_regenerate_id();
-}
+//if(session_status() === PHP_SESSION_ACTIVE ){
+//	session_regenerate_id();
+//}
 define('_GaiaEXEC', 1);
 
 require_once(str_replace('\\', '/', dirname(dirname(__FILE__))) . '/registry.php');

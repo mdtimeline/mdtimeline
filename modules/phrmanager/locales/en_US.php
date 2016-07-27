9cb61d4d713d8aa1455059dc17ae02f43b5bd542
Gino Rivera <hrivera@tranextgen.com>
2016-07-27 17:05:27 -0400

    Removed folder from repository

--- modules/phrmanager/locales/en_US.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Words that are reserved in javascript.
-// - delete
-// - new
-// Please, use the following format _('save') not i18n.save
-// JavaScript has reserved variables after the dot.
-$LANG = array(
-    'patient_messages'  =>  'Patient Messages'
-);

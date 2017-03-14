1604e319190e276b8133e1b3d53e6990473f9a72
Gino Rivera <hrivera@tranextgen.com>
2017-03-14 09:16:35 -0400

    Drug to Drug and Drug to Allergies Interaction: - Modified file copyright comments

--- modules/reportcenter/dataProvider/ReportList.php
+++ modules/reportcenter/dataProvider/ReportList.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * GaiaEHR (Electronic Health Records)
- * Copyright (C) 2015 TRA NextGen, Inc.
+ * mdTimeLine EHR (Electronic Health Records)
+ * Copyright (C) 2017 mdTImeLine, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

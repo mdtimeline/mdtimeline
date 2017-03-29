841a457e2ed3021b150424f432ee6f794f3aeece
Gino Rivera <hrivera@tranextgen.com>
2017-03-29 09:04:29 -0400

    Medication Reconciliation: - App compile

--- app/app.js
+++ app/app.js
@@ -41884,6 +41884,13 @@ Ext.define('App.controller.patient.CCDImport', {
             });
         }
 
+        AuditLog.addLog({
+            pid: pid,
+            uid: app.user.id,
+            foreign_table: 'IMPORT',
+            event: 'Patient C-CDA IMPORT'
+        });
+
 	},
 
 	addCdaToPatientDocument: function (pid) {

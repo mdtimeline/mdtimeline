27539a26bc969d3e5507ba9fee36b7b65b927924
Ernesto J Rodriguez <vela1606@gmail.com>
2017-07-11 21:50:45 -0400

    Allow international codes

--- app/ux/VTypes.js
+++ app/ux/VTypes.js
@@ -116,7 +116,7 @@ Ext.apply(Ext.form.VTypes, {
 	// Validate for an valid Phone Number
 	// ---------------------------------------
 	phoneNumber: function(val, field){
-		var rgx = /^(\+1)?[- ]?\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+		var rgx = /^(\+\d{1,3}[- ]?)?\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
 		return val.match(rgx);
 	},
 	phoneNumberText: _('vtype_phoneNumber'),

c915482801526beffa83b5db8d4450105ba57ce6
Ernesto J Rodriguez <vela1606@gmail.com>
2017-10-26 12:24:37 -0400

    Single sign on

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '1.0.508');
+	define('VERSION', '1.0.509');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

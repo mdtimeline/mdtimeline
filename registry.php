0689cb0fea22357db1ae39bd977ce8ef557a76a8
Ernesto J Rodriguez <vela1606@gmail.com>
2018-02-26 06:30:30 -0400

    app.getDate

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '1.0.700');
+	define('VERSION', '1.0.701');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

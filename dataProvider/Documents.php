324322db8af18572df9e36defbdefcfb541b0108
Ernesto J Rodriguez <vela1606@gmail.com>
2018-03-13 22:28:19 -0400

    UTF-8 Fix

--- dataProvider/Documents.php
+++ dataProvider/Documents.php
@@ -595,7 +595,7 @@ class Documents {
 				function_exists('mb_detect_encoding') &&
 				mb_detect_encoding($page, 'UTF-8', false)
 			){
-				$page = iconv('UTF-8', 'ISO-8859-5', $page);
+				$page = iconv('UTF-8', 'ISO-8859-5//IGNORE', $page);
 			}
 
 			if($this->isHtml($page)){

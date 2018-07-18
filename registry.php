382291684e6dff800125697470b7e84ed21331be
Ernesto J Rodriguez <vela1606@gmail.com>
2018-07-17 21:20:16 -0400

    Users Print

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '1.0.916');
+	define('VERSION', '1.0.917');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

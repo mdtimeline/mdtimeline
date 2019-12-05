8405f318b024e60937b87bffe6631691b743f020
Ernesto J Rodriguez <vela1606@gmail.com>
2019-12-04 21:17:06 -0400

    store procedure

--- dataProvider/MeasureCalculation.php
+++ dataProvider/MeasureCalculation.php
@@ -336,67 +336,8 @@ class MeasureCalculation {
 		 * Denominator: Number of patients seen by the EP.
 		 */
 
-		$sql = <<<SQL
-DROP TABLE IF EXISTS g2_report_ds;
-DROP TABLE IF EXISTS g2_report_ds_first_encounters;
-DROP TABLE IF EXISTS g2_report_denominator_ds;
-DROP TABLE IF EXISTS g2_report_numerator_ds;
-
-CREATE TEMPORARY TABLE g2_report_ds
-SELECT 
-    r.eid,
-    r.pid,
-    r.provider,
-    r.facility,
-    r.in_time,
-	r.service_date,
-    r.event_id,
-    r.event_date
-FROM
-    (SELECT 
-        e.eid,
-        e.provider_uid as provider,
-        e.facility,
-            e.pid,
-            e.service_date,
-            a.id AS event_id,
-            a.event_date,
-            IF(e.service_date IS NOT NULL
-                AND CAST(CONCAT(DATE(a.event_date), ' 23:59:59') AS DATETIME) < DATE_ADD(e.service_date, INTERVAL 48 HOUR), 1, 0) AS in_time
-    FROM
-        encounters AS e
-    LEFT JOIN audit_log AS a ON a.pid = e.pid AND e.eid = a.eid AND a.event IN ('CCDA_RECEIVED')
-    WHERE
-        e.provider_uid IN ('{$provider_id}')
-             AND e.service_date IS NOT NULL
-             AND e.service_date BETWEEN CAST('{$start_date}' AS DATE) AND CAST('{$end_date}' AS DATE)
-           
-	) r ORDER BY r.service_date;
-
-CREATE TEMPORARY TABLE g2_report_ds_first_encounters SELECT eid, pid,provider,facility FROM g2_report_ds GROUP BY pid, provider;
-
-CREATE TEMPORARY TABLE g2_report_denominator_ds
- SELECT 1 as `value`, pid FROM g2_report_ds GROUP BY pid;
-
-CREATE TEMPORARY TABLE g2_report_numerator_ds
-  SELECT 1 as `value`, pid FROM g2_report_ds WHERE in_time = '1' AND eid IN (SELECT eid FROM g2_report_ds_first_encounters) group by pid;
-
-INSERT INTO g2_report_numerator_ds 
-  SELECT -1 as `value`, pid FROM g2_report_ds WHERE in_time = '0' AND eid NOT IN (SELECT eid FROM g2_report_ds_first_encounters) group by pid;
-
-SET @denominator = (SELECT sum(`value`) FROM g2_report_denominator_ds);
-SET @numerator = (SELECT sum(`value`) FROM g2_report_numerator_ds);
-
-SELECT 
-	@denominator as denominator,
-	(SELECT group_concat(pid) FROM (SELECT pid FROM g2_report_denominator_ds GROUP BY pid) as d) as denominator_pids,
-	@numerator as numerator,
-    (SELECT group_concat(pid) FROM (SELECT pid FROM g2_report_numerator_ds GROUP BY pid) as n) as numerator_pids
-
-SQL;
-
-		$sth = $this->conn->prepare($sql);
-		$sth->execute();
+		$sth = $this->conn->prepare("CALL `getProvidePatientsElectronicAccessReportByDates`(?, ?, ?);");
+		$sth->execute([$provider_id, $start_date, $end_date]);
 		$report =  $sth->fetch(PDO::FETCH_ASSOC);
 
 		$records[] = [

385c9ea3e4764c3020c57b08f39fd58d8820b873
Ernesto J Rodriguez <vela1606@gmail.com>
2019-12-14 07:57:23 -0400

    bump

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.1032');
+	define('VERSION', '2.0.1033');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

54e44b3c0b9615a248dd046106836bef6ce76a50
mdtimeline <nelson.rosa3@gmail.com>
2020-12-01 04:30:28 -0400

    active specialties all consola font added

--- app/app.js
+++ app/app.js
@@ -64997,11 +64997,28 @@ Ext.define('App.ux.combo.ActiveSpecialties', {
 	editable: false,
 	emptyText: _('select'),
 	queryMode: 'local',
-	store: Ext.create('App.store.administration.Specialties',{
-		pageSize: 500,
-		autoLoad: true
-	}),
+	includeAllOption: false,
 	initComponent: function(){
+		var me = this;
+
+		this.store = Ext.create('App.store.administration.Specialties',{
+			pageSize: 500,
+			autoLoad: true,
+			listeners: {
+				load: function (){
+					if(me.includeAllOption){
+						me.store.insert(0,{
+							id: -1,
+							title: 'ALL',
+							combo_text: 'ALL',
+							active: true
+						});
+						me.setValue(-1);
+					}
+				}
+			}
+		});
+
 		this.store.clearFilter(true);
 		this.store.filter([
 			{
--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.1113');
+	define('VERSION', '2.0.1114');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

91acd24cf7a0bda2e354fb4edef20ce1be59a14a
mdtimeline <nelson.rosa3@gmail.com>
2020-12-27 19:09:53 -0400

    CareTeam Legal Letter Progress Bump

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.1119a');
+	define('VERSION', '2.0.1120');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

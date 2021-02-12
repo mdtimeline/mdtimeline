b9d3b2759552389b98b7101ce12cb0d6d30f21ca
mdtimeline <nelson.rosa3@gmail.com>
2021-02-11 20:29:42 -0400

    bump

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.1128');
+	define('VERSION', '2.0.1129');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

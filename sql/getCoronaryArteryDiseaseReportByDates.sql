b2935ab730b06c0de4aff4f779d2256ecca3f2dd
mdtimeline <ecruz@mdtimeline.com>
2021-04-21 09:34:50 -0400

    Heart Failure (HF): Angiotensin-Converting Enzyme (ACE) Inhibitor or Angiotensin Receptor Blocker (ARB) or Angiotensin Receptor-Neprilysin Inhibitor (ARNI) Therapy for Left Ventricular Systolic Dysfunction (LVSD) - query fix

--- sql/getCoronaryArteryDiseaseReportByDates.sql
+++ sql/getCoronaryArteryDiseaseReportByDates.sql
@@ -37,12 +37,18 @@ BEGIN
                                             '349199', '349200', '349201', '349353', '349373', '349401', '349405', '349483', '351292', '351293', '403853', '403854', '403855',
                                             '477130', '485471', '577776', '578325', '578330', '636042', '636045', '639537', '722126', '722131', '722134', '722137', '730861',
                                             '730866', '730869', '730872', '802749', '845488', '848131', '848135', '848140', '848145', '848151', '854925', '854984', '854988',
-                                            '857166', '857169', '857174', '857183', '857187', '858804', '858810', '858813', '858817', '858824', '858828', '876514', '876519',
+                                            '857166', '857169', '857174', '857183', '857187', '858804', '858810', '858813', '858815', '858817', '858824', '858828', '876514', '876519',
                                             '876524', '876529', '897781', '897783', '897844', '897853', '898342', '898346', '898350', '898353', '898356', '898359', '898362',
                                             '898367', '898372', '898378', '898687', '898690', '898719', '898723', '979464', '979468', '979471', '979480', '979485', '979492',
                                             '999967', '999986', '999991', '999996')
                             AND ((m.date_ordered BETWEEN start_date AND end_date) OR (m.is_active AND m.begin_date BETWEEN DATE_SUB(start_date, INTERVAL 1 YEAR) AND end_date))),
-                    '1', '0') as was_prescribed_or_active
+                    '1', '0') as was_prescribed_or_active,
+                IF( EXISTS (
+                            SELECT * FROM encounter_dx as edx
+                            WHERE edx.pid = e.pid
+                              AND edx.dx_type = 'F'
+                              AND edx.code = 'I50.1'),
+                    '1', '0') as has_or_had_lvef_dx
             FROM
                 (SELECT enc.eid,
                         enc.pid,
@@ -79,33 +85,44 @@ BEGIN
                                             '349199', '349200', '349201', '349353', '349373', '349401', '349405', '349483', '351292', '351293', '403853', '403854', '403855',
                                             '477130', '485471', '577776', '578325', '578330', '636042', '636045', '639537', '722126', '722131', '722134', '722137', '730861',
                                             '730866', '730869', '730872', '802749', '845488', '848131', '848135', '848140', '848145', '848151', '854925', '854984', '854988',
-                                            '857166', '857169', '857174', '857183', '857187', '858804', '858810', '858813', '858817', '858824', '858828', '876514', '876519',
+                                            '857166', '857169', '857174', '857183', '857187', '858804', '858810', '858813', '858815', '858817', '858824', '858828', '876514', '876519',
                                             '876524', '876529', '897781', '897783', '897844', '897853', '898342', '898346', '898350', '898353', '898356', '898359', '898362',
                                             '898367', '898372', '898378', '898687', '898690', '898719', '898723', '979464', '979468', '979471', '979480', '979485', '979492',
                                             '999967', '999986', '999991', '999996')
                               AND ((m.date_ordered BETWEEN start_date AND end_date) OR (m.is_active AND m.begin_date BETWEEN DATE_SUB(start_date, INTERVAL 1 YEAR) AND end_date))),
-                    '1', '0') as was_prescribed_or_active
+                    '1', '0') as was_prescribed_or_active,
+                IF( EXISTS (
+                            SELECT * FROM encounter_dx as edx
+                            WHERE edx.pid = e.pid
+                            AND edx.dx_type = 'F'
+                            AND edx.code = 'I50.1'),
+                    '1', '0') as has_or_had_lvef_dx
             FROM
-                (SELECT enc.eid,
-                        enc.pid,
-                        YEAR(enc.service_date) - YEAR(p.DOB) - (RIGHT(enc.service_date, 5) < RIGHT(p.DOB, 5)) as age,
-                        COUNT(*) AS total
+                (SELECT enc.pid, enc.eid
                  FROM encounters as enc
-                          INNER JOIN patient p ON enc.pid = p.pid
-                          INNER JOIN encounter_dx AS dx ON enc.eid = dx.eid
+                          JOIN (SELECT enc.pid, COUNT(*) AS total
+                                FROM encounters as enc
+                                         INNER JOIN patient p ON enc.pid = p.pid
+                                WHERE enc.provider_uid = provider_id
+                                  AND enc.service_date BETWEEN start_date AND end_date
+                                  AND YEAR(enc.service_date) - YEAR(p.DOB) - (RIGHT(enc.service_date, 5) < RIGHT(p.DOB, 5)) >= 18
+                                GROUP BY enc.pid
+                                HAVING  total >= 2) as t
+                               ON enc.pid = t.pid
+                          INNER JOIN encounter_dx as edx
+                                     ON enc.eid = edx.eid
+                                         AND enc.pid = edx.pid
                  WHERE enc.provider_uid = provider_id
                    AND enc.service_date BETWEEN start_date AND end_date
-                   AND dx.code IN ('I11.0', 'I13.0', 'I13.2', 'I50.1', 'I50.20', 'I50.21', 'I50.22', 'I50.23', 'I50.30', 'I50.31',  'I50.32',
-                                   'I50.33', 'I50.40', 'I50.41', 'I50.42', 'I50.43', 'I50.814', 'I50.82', 'I50.83', 'I50.84', 'I50.89', 'I50.9')
-                 GROUP BY enc.pid
-                 HAVING  total >= 2) e
-            WHERE e.age >= 18;
+                   AND edx.code IN ('I50', 'I50.2', 'I50.20', 'I50.21', 'I50.22', 'I50.23', 'I50.3', 'I50.30', 'I50.31',
+                                    'I50.32', 'I50.33', 'I50.4', 'I50.40', 'I50.41', 'I50.42', 'I50.43', 'I50.8', 'I11.0',
+                                    'I09.81', 'I13.0', 'I13.2')) e;
 
         END IF;
 
 
         CREATE TEMPORARY TABLE report_denominator_ds
-        SELECT 1 as `value`, pid FROM report_ds  GROUP BY pid;
+        SELECT 1 as `value`, pid FROM report_ds WHERE has_or_had_lvef_dx = '1' GROUP BY pid;
 
         CREATE TEMPORARY TABLE report_numerator_ds
         SELECT 1 as `value`, pid FROM report_ds WHERE was_prescribed_or_active = '1' GROUP BY pid;

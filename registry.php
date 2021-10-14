b01d0309f1cd3684f2c9a2c1acd232b4fa6b81d2
Ernesto Rodriguez <erodriguez@mdtimeline.com>
2021-10-14 12:46:10 -0400

    RXNORM NDC

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.1152');
+	define('VERSION', '2.0.1153');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

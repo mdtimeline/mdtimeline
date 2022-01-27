ae8d0f740620eab0e8a6ea6bc8aebf49514f28b4
mdtimeline <ecruz@mdtimeline.com>
2022-01-26 21:40:47 -0400

    Bump

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.2007');
+	define('VERSION', '2.0.2008');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

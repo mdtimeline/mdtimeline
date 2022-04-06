1dc76b83a58fe2920f6efec9d2fc19b9614d9db8
Ernesto Rodriguez <erodriguez@mdtimeline.com>
2022-04-06 05:26:02 -0400

    bump

--- registry.php
+++ registry.php
@@ -52,7 +52,7 @@ if(!defined('URL')){
 
 // application version
 if(!defined('VERSION'))
-	define('VERSION', '2.0.2018');
+	define('VERSION', '2.0.2019');
 // extjs sdk directory
 if(!defined('EXTJS'))
 	define('EXTJS', 'extjs-4.2.1');

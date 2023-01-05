367192ebc2f75ff1ce739985bc8a7238a91816f9
Ernesto Rodriguez <erodriguez@mdtimeline.com>
2023-01-04 21:59:08 -0400

    File manager include error

--- classes/FileManager.php
+++ classes/FileManager.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-include_once ('../session.php');
+include_once (ROOT.'/session.php');
 
 class FileManager
 {
